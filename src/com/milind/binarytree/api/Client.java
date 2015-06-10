1b35dc9604ab49bcae594d03706fb26e71b0cac7
milindatgithub <mailtomilind@gmail.com>
2015-06-10 16:40:07 +0530

    Commit For the creating traversal

--- src/com/milind/binarytree/api/Client.java
+++ src/com/milind/binarytree/api/Client.java
@@ -1,7 +1,67 @@
 package com.milind.binarytree.api;
 
+import com.milind.binarytree.node.Node;
+
 /**
  * Created by deobhank on 6/9/2015.
  */
 public class Client {
+
+    public static  void  main(String args[]){
+
+        Node root = new Node("A");
+        TreeCreationApi crApi = new TreeCreationApi(root);
+        crApi.insertNode(new Node("B"));
+        crApi.insertNode(new Node("C"));
+        crApi.insertNode(new Node("D"));
+        crApi.insertNode(new Node("E"));
+        crApi.insertNode(new Node("F"));
+        crApi.insertNode(new Node("G"));
+        crApi.insertNode(new Node("H"));
+        crApi.insertNode(new Node("I"));
+        crApi.insertNode(new Node("J"));
+        crApi.insertNode(new Node("K"));
+        crApi.insertNode(new Node("L"));
+        crApi.insertNode(new Node("M"));
+        crApi.insertNode(new Node("N"));
+        crApi.insertNode(new Node("O"));
+        crApi.insertNode(new Node("P"));
+        crApi.insertNode(new Node("Q"));
+        crApi.insertNode(new Node("R"));
+        crApi.insertNode(new Node("S"));
+        crApi.insertNode(new Node("T"));
+        crApi.insertNode(new Node("U"));
+        crApi.insertNode(new Node("V"));
+        crApi.insertNode(new Node("W"));
+        crApi.insertNode(new Node("X"));
+        crApi.insertNode(new Node("Y"));
+        crApi.insertNode(new Node("Z"));
+        crApi.insertNode(new Node("A1"));
+        crApi.insertNode(new Node("A2"));
+        crApi.insertNode(new Node("A3"));
+        crApi.insertNode(new Node("A4"));
+        crApi.insertNode(new Node("A5"));
+        //crApi.insertNode(new Node("A6"));
+
+        new DisplayTreeApi().displayTree(root);
+        TreeApi treeApi = new TreeApi();
+        int treedepth = treeApi.depthOfTree(root,0);
+        System.out.println("Depth of Tree: "+treedepth);
+
+
+        TreeTraverseApi treeTraverseApi = new TreeTraverseApi();
+        System.out.print("Pre-Order Traverse: ");
+        treeTraverseApi.traversePreOrder(root);
+        System.out.println("");
+        System.out.print("In-Order Traverse: ");
+        treeTraverseApi.traverseInOrder(root);
+        System.out.println("");
+        System.out.print("Post-Order Traverse: ");
+        treeTraverseApi.traversePostOrder(root);
+        System.out.println("");
+        System.out.print("Traverse Breadth Wise: ");
+        treeTraverseApi.traverseBreadthWise(root);
+        System.out.println("");
+        System.out.println(" Number of Nodes: "+treeApi.countNode(root));
+    }
 }
--- src/com/milind/binarytree/api/TreeApi.java
+++ src/com/milind/binarytree/api/TreeApi.java
@@ -1,7 +1,43 @@
 package com.milind.binarytree.api;
 
+import com.milind.binarytree.node.Node;
+
 /**
  * Created by deobhank on 6/10/2015.
  */
 public class TreeApi {
+
+    /**
+     * This will find out the depth of the tree
+     * @param root
+     * @param depth
+     * @return
+     */
+    public int depthOfTree(Node root, int depth){
+
+        int leftdepth = depth;
+        int rightdepth = depth;
+        if(root.getLeft() !=null){
+            leftdepth = depthOfTree(root.getLeft(),depth+1);
+        }
+        else if(root.getRight() !=null){
+
+            rightdepth = depthOfTree(root.getRight(), depth+1);
+        }
+        return (leftdepth > rightdepth) ? leftdepth : rightdepth;
+    }
+
+    /**
+     * This will count the number of nodes
+     * @param root
+     * @return
+     */
+    public int countNode(Node root){
+
+        if(root == null){
+            return 0;
+        } else {
+            return countNode(root.getLeft())+1+ countNode(root.getRight());
+        }
+    }
 }
